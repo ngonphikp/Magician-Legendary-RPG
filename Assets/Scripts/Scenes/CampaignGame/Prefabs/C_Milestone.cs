0147290d043dbf992940f44feed41d1fade39c39
phi <ngonphikp@gmail.com>
2020-05-29 18:13:35 +0700

    Demo Anim T and M + Campaign UI

---
+++ Assets/Scripts/Scenes/CampaignGame/Prefabs/C_Milestone.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class C_Milestone : MonoBehaviour
+{
+    private int star = 0;
+
+    [SerializeField]
+    private Sprite[] spriteStars = null;
+
+    private Image img;
+
+    private void Awake()
+    {
+        img = this.GetComponent<Image>();
+    }
+
+    public void Click()
+    {
+        Debug.Log("Click :" + this.gameObject.name);
+    }
+
+    public void SetStar(int star)
+    {
+        if (this.star == star) return;
+        this.star = star;
+        if(star <= spriteStars.Length)
+        {
+            img.sprite = spriteStars[star];
+        }
+        else
+        {
+            Debug.LogWarning("Exits Sprite");
+        }
+    }
+}
